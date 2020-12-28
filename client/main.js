254c66ec69acce4e56e13626b220426dedd360ca
DanielGoldenberg <goldenbergdaniel8@gmail.com>
2020-12-28 17:49:29 -0500

    Changes

--- client/main.js
+++ client/main.js
@@ -1,40 +1,38 @@
-const URL = "https://teachablemachine.withgoogle.com/models/je3roTdIx/";
+const URL = "https://teachablemachine.withgoogle.com/models/je3roTdIx/"
 
-let model, webcam, labelContainer, maxPredictions;
+let model, webcam, labelContainer, maxPredictions
 
 async function init() {
-    const modelURL = URL + "model.json";
-    const metadataURL = URL + "metadata.json";
-
+    const modelURL = URL + "model.json"
+    const metadataURL = URL + "metadata.json"
   
-    model = await tmImage.load(modelURL, metadataURL);
-    maxPredictions = model.getTotalClasses();
-
+    model = await tmImage.load(modelURL, metadataURL)
+    maxPredictions = model.getTotalClasses()
   
-    const flip = true; 
-    webcam = new tmImage.Webcam(200, 200, flip); 
-    await webcam.setup();
-    await webcam.play();
-    window.requestAnimationFrame(loop);
+    const flip = true
+    webcam = new tmImage.Webcam(200, 200, flip)
+    await webcam.setup()
+    await webcam.play()
+    window.requestAnimationFrame(loop)
 
-    document.getElementById("webcam-container").appendChild(webcam.canvas);
-    labelContainer = document.getElementById("label-container");
+    document.getElementById("webcam-container").appendChild(webcam.canvas)
+    labelContainer = document.getElementById("label-container")
     for (let i = 0; i < maxPredictions; i++) { 
-        labelContainer.appendChild(document.createElement("div"));
+        labelContainer.appendChild(document.createElement("div"))
     }
 }
 
 async function loop() {
-    webcam.update(); 
-    await predict();
-    window.requestAnimationFrame(loop);
+    webcam.update() 
+    await predict()
+    window.requestAnimationFrame(loop)
 }
 
   async function predict() {
-     const prediction = await model.predict(webcam.canvas);
+    const prediction = await model.predict(webcam.canvas)
     for (let i = 0; i < maxPredictions; i++) {
         const classPrediction =
-            prediction[i].className + ": " + prediction[i].probability.toFixed(2);
-        labelContainer.childNodes[i].innerHTML = classPrediction;
+            prediction[i].className + ": " + prediction[i].probability.toFixed(2)
+        labelContainer.childNodes[i].innerHTML = classPrediction
     }
 }
