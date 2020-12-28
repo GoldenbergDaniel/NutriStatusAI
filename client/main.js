5e816bec9940893de09c894d2ffb6913a66ca661
DanielGoldenberg <goldenbergdaniel8@gmail.com>
2020-12-28 18:44:01 -0500

    Client sends request to server

--- client/main.js
+++ client/main.js
@@ -1,6 +1,10 @@
-const URL = "https://teachablemachine.withgoogle.com/models/je3roTdIx/"
+const URL = "https://teachablemachine.withgoogle.com/models/je3roTdIx/" // classified information
 
 let model, webcam, labelContainer, maxPredictions
+
+let email
+let userName
+let foodName
 
 async function init() {
     const modelURL = URL + "model.json"
@@ -10,14 +14,16 @@ async function init() {
     maxPredictions = model.getTotalClasses()
   
     const flip = true
+
     webcam = new tmImage.Webcam(200, 200, flip)
     await webcam.setup()
     await webcam.play()
 
     window.requestAnimationFrame(loop)
-
     document.getElementById("webcam-container").appendChild(webcam.canvas)
+
     labelContainer = document.getElementById("label-container")
+
     for (let i = 0; i < maxPredictions; i++) { 
         labelContainer.appendChild(document.createElement("div"))
     }
@@ -31,35 +37,67 @@ async function loop() {
 
 async function predict() {
     const prediction = await model.predict(webcam.canvas)
+
     for (let i = 0; i < maxPredictions; i++) {
-        const classPrediction =
-            prediction[i].className + ": " + prediction[i].probability.toFixed(2)
+        const classPrediction = prediction[i].className + ": " + prediction[i].probability.toFixed(2)
+
         labelContainer.childNodes[i].innerHTML = classPrediction
+
+        if (prediction[0].probability.toFixed(2) == 0.85) {
+            foodName = "egg"
+        }
+
+        if (prediction[1].probability.toFixed(2) == 0.85) {
+            foodName = "cane"
+        }
+
+        if (prediction[3].probability.toFixed(2) == 0.85) {
+            window.alert(predicton[2])
+            foodName = "yogurt"
+        }
     }
 }
 
-//CHART
+function saveFood() {
+    var url = `http://localhost:8080/api/?
+        email=${encodeURIComponent(email)}&
+        name=${encodeURIComponent(userName)}&
+        food-name=${encodeURIComponent(foodName)}`
+
+    var request = new XMLHttpRequest()
+    request.open("GET", url)
+    request.onload = () => {
+        // var response = JSON.parse(request.responseText) 
+        // document.querySelector("#output").querySelector("h3").innerText = response
+    }
+    
+    request.send()
+}
+
+// CHART
 var dData = function() {
     return Math.round(Math.random() * 90) + 10
-  }
-  
-  var barChartData = {
+}
+
+var barChartData = {
     labels: ["dD 1", "dD 2", "dD 3", "dD 4", "dD 5", "dD 6", "dD 7", "dD 8", "dD 9", "dD 10"],
     datasets: [{
-      fillColor: "rgba(0,60,100,1)",
-      strokeColor: "black",
-      data: [dData(), dData(), dData(), dData(), dData(), dData(), dData(), dData(), dData(), dData()]
+        fillColor: "rgba(0, 60, 100, 1)",
+        strokeColor: "black",
+        data: [dData(), dData(), dData(), dData(), dData(), dData(), dData(), dData(), dData(), dData()]
     }]
-  }
-  
-  var index = 11;
-  var ctx = document.getElementById("canvas").getContext("2d");
-  var barChartDemo = new Chart(ctx).Bar(barChartData, {
-    responsive: true,
-    barValueSpacing: 2
-  });
-  setInterval(function() {
+}
+
+var index = 11
+var ctx = document.getElementById("canvas").getContext("2d")
+
+var barChartDemo = new Chart(ctx).Bar(barChartData, {
+responsive: true,
+barValueSpacing: 2
+})
+
+setInterval(function() {
     barChartDemo.removeData()
     barChartDemo.addData([dData()], "dD " + index)
     index++
-  }, 3000)
+}, 3000)
